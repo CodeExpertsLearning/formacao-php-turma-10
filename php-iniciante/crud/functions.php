13654b6c7b5ef2b5c0b4f6fceae7a53e8862e665
Nanderson Castro <nandokstro@gmail.com>
2020-10-24 12:17:06 -0300

    Adicionando conteudo CRUD

--- php-iniciante/crud/functions.php
+++ php-iniciante/crud/functions.php
@@ -11,7 +11,9 @@ function hello($name = 'Teste')
 function pdo()
 {
    $connector = 'mysql:dbname=' . DB_NAME . ';host=' . DB_HOST;
+   
    $conn = new PDO($connector, DB_USER, DB_PASSWORD);
+   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
    return $conn;
 }
@@ -19,6 +21,17 @@ function pdo()
 function read_db($conn, $table, $mode = PDO::FETCH_ASSOC)
 {
     return $conn->query('SELECT * FROM ' . $table, $mode);
+}
+
+function read_one_db($conn, $table, $id, $mode = PDO::FETCH_ASSOC)
+{
+    $sql = 'SELECT * FROM users WHERE id = :id';
+
+    $select = $conn->prepare($sql);
+    $select->bindValue(':id', $id, PDO::PARAM_INT);
+    $select->execute();
+
+    return $select->fetch($mode);
 }
 
 
@@ -42,10 +55,40 @@ function create_db($conn, $table, $data = [])
                 ) 
     ";
 
+    //Prepared Statements
     $insert = $conn->prepare($sql);
     $insert->bindValue(':name', $data['name'], PDO::PARAM_STR);
     $insert->bindValue(':email', $data['email'], PDO::PARAM_STR);
     $insert->bindValue(':password', $data['password'], PDO::PARAM_STR);
 
     return $insert->execute();
+}
+
+function update_db($conn, $table, $data) 
+{
+    $sql = '
+        UPDATE users 
+        SET name = :name, email = :email, password = :password
+        WHERE id = :id
+    ';
+
+    $update = $conn->prepare($sql);
+    $update->bindValue(':name', $data['name'], PDO::PARAM_STR);
+    $update->bindValue(':email', $data['email'], PDO::PARAM_STR);
+    $update->bindValue(':password', $data['password'], PDO::PARAM_STR);
+
+    $update->bindValue(':id', $data['id'], PDO::PARAM_INT);
+
+    return $update->execute();
+}
+
+function delete_db($conn, $table, $id)
+{
+    $sql = 'DELETE FROM users WHERE id = :id';
+
+    $delete = $conn->prepare($sql);
+
+    $delete->bindValue(':id', $id, PDO::PARAM_INT);
+
+    return $delete->execute();
 }
--- php-iniciante/crud/index.php
+++ php-iniciante/crud/index.php
@@ -18,7 +18,6 @@ if($url == '/admin/usuario') {
 if($url == '/admin/usuario/criar') {
 
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
-     
         //Processar a criação do usuário...
         $data = $_POST;
         $data['password'] = password_hash($data['password'], PASSWORD_ARGON2I);
@@ -28,5 +27,43 @@ if($url == '/admin/usuario/criar') {
         return header('Location: /admin/usuario');
     }
 
-    require __DIR__ . '/templates/create.phtml';
-} 
+   require __DIR__ . '/templates/create.phtml';
+}
+
+if ($url == '/admin/usuario/editar') {
+
+
+    $id = (int) $_GET['id'];
+
+    $user = read_one_db(pdo(), 'users', $id);
+
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //Processar a criação do usuário...
+        $data = $_POST;
+
+        if($data['password'])    {
+            $data['password'] = password_hash($data['password'], PASSWORD_ARGON2I);
+        }else {
+            $data['password'] = $user['password'];
+        }
+        
+        $data['id'] = $id;
+
+        update_db(pdo(), 'users', $data);
+
+        return header('Location: /admin/usuario');
+    }
+    
+    require __DIR__ . '/templates/edit.phtml';
+}
+
+
+if($url == '/admin/usuario/remover') {
+    
+    $id = (int) $_GET['id'];
+
+    delete_db(pdo(), 'users', $id);
+
+    return header('Location: /admin/usuario');
+}
